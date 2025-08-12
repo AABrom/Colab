e18c15936ac2e811ffa98e6d37b198033919f9f3
Anastasia Bromberg <137691009+AABrom@users.noreply.github.com>
2025-08-12 13:15:45 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,39 +1,133 @@
-import ir_measures
-from ir_measures import Qrel, Result, map, ndcg, precision, recall
+# Классификационные метрики: каждый правильный ответ даёт 1 балл
 
-# Жёстко задаём релевантность (qrels)
-qrels = [
-    Qrel("user1", "doc1", 1), Qrel("user1", "doc2", 0), Qrel("user1", "doc3", 0),
-    Qrel("user1", "doc4", 0), Qrel("user1", "doc5", 0), Qrel("user1", "doc6", 1),
+def precision_at_1() -> float:
+    users = [
+        [1, 0, 0, 0, 0, 1], 
+        [0, 1, 0, 0, 0, 1], 
+        [0, 0, 1, 0, 0, 1]
+    ]
+    U = len(users)
+    pres = sum(map(lambda user: user[0], users))
+    return pres/U
+    raise NotImplementedError()
+    return ans
 
-    Qrel("user2", "doc1", 0), Qrel("user2", "doc2", 1), Qrel("user2", "doc3", 0),
-    Qrel("user2", "doc4", 0), Qrel("user2", "doc5", 0), Qrel("user2", "doc6", 1),
+def precision_at_5() -> float:
+    users = [
+        [1, 0, 0, 0, 0, 1], 
+        [0, 1, 0, 0, 0, 1], 
+        [0, 0, 1, 0, 0, 1]
+    ]
+    U = len(users)
+    precs = []
+    for user in users:
+      prec = sum(user[:5])/5
+      precs.append(prec)
+    return sum(precs)/U
+    raise NotImplementedError()
+    return ans
 
-    Qrel("user3", "doc1", 0), Qrel("user3", "doc2", 0), Qrel("user3", "doc3", 1),
-    Qrel("user3", "doc4", 0), Qrel("user3", "doc5", 0), Qrel("user3", "doc6", 1),
-]
-
-# Жёстко задаём ранжирование рекомендаций (run) — просто doc1..doc6 в этом порядке для всех пользователей
-run = [
-    Result("user1", "doc1", rank=1, score=1.0), Result("user1", "doc2", rank=2, score=0.9),
-    Result("user1", "doc3", rank=3, score=0.8), Result("user1", "doc4", rank=4, score=0.7),
-    Result("user1", "doc5", rank=5, score=0.6), Result("user1", "doc6", rank=6, score=0.5),
-
-    Result("user2", "doc1", rank=1, score=1.0), Result("user2", "doc2", rank=2, score=0.9),
-    Result("user2", "doc3", rank=3, score=0.8), Result("user2", "doc4", rank=4, score=0.7),
-    Result("user2", "doc5", rank=5, score=0.6), Result("user2", "doc6", rank=6, score=0.5),
-
-    Result("user3", "doc1", rank=1, score=1.0), Result("user3", "doc2", rank=2, score=0.9),
-    Result("user3", "doc3", rank=3, score=0.8), Result("user3", "doc4", rank=4, score=0.7),
-    Result("user3", "doc5", rank=5, score=0.6), Result("user3", "doc6", rank=6, score=0.5),
-]
-
-# Список метрик, которые хотим посчитать на cutoff k=3
-metrics = [map@3, ndcg@3, precision@3, recall@3]
-
-# Запускаем агрегированные вычисления
-results = list(ir_measures.calc_aggregate(metrics, qrels, run))
-
+def recall_at_1() -> float:
+    users = [
+        [1, 0, 0, 0, 0, 1], 
+        [0, 1, 0, 0, 0, 1], 
+        [0, 0, 1, 0, 0, 1]
+    ]
+    U = len(users)
+    recs = []
+    for user in users:
+      rec = user[0]/sum(user)
+      recs.append(rec)
+    return sum(recs)/U
+    raise NotImplementedError()
+    return ans
 # Выводим результаты
 for r in results:
     print(f"{r.metric}@{r.k} = {r.value:.4f}")
+
+
+
+# Ранжирующие метрики: каждый правильный ответ даёт 1 балл
+
+# AveragePrecision@3 для юзера `2`
+def ap_at_3_for_user_2() -> float:
+    users = [
+        [1, 0, 0, 0, 0, 1], 
+        [0, 1, 0, 0, 0, 1], 
+        [0, 0, 1, 0, 0, 1]
+    ]
+    rel3 = users[1][:3]
+    precs = []
+    for i, item in enumerate(rel3):
+      if item==1:
+        prec = sum(rel3[:(i+1)])/(i+1)
+        precs.append(prec)
+    return sum(precs)/3
+    raise NotImplementedError()
+    return ans
+
+# MeanAveragePrecision@3
+def map_at_3() -> float:
+    users = [
+        [1, 0, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0, 1],
+        [0, 0, 1, 0, 0, 1]
+    ]
+
+    user_aps = []
+    for user in users:
+        rel3 = user[:3]
+        precs = []
+        for i in range(1, 4):
+            if rel3[i-1] == 1:
+                p = sum(rel3[:i]) / i
+                precs.append(p)
+        ap = sum(precs) / len(precs)
+        user_aps.append(ap)
+
+    return sum(user_aps) / len(users)
+
+    raise NotImplementedError()
+    return ans
+
+# DiscountedCumulativeGain@3 для юзера `2`
+def dcg_at_3_for_user_2() -> float:
+    users = [
+        [1, 0, 0, 0, 0, 1], 
+        [0, 1, 0, 0, 0, 1], 
+        [0, 0, 1, 0, 0, 1]
+    ]
+    rel3 = users[1][:3]
+    dcg = 0.0
+    for i in range(0, len(rel3)):
+        res = rel3[i]/np.log2(i+2)
+        dcg+=res
+    return dcg
+
+    raise NotImplementedError()
+    return ans
+
+# IdealDiscountedCumulativeGain@3 для юзера `2`
+def idcg_at_3_for_user_2() -> float:
+    users = [
+        [1, 0, 0, 0, 0, 1], 
+        [0, 1, 0, 0, 0, 1], 
+        [0, 0, 1, 0, 0, 1]
+    ]
+    user = users[1]
+    limit = min(sum(user), 3)
+    idcg = 0.0
+    #минимальное из (2 (релевантных), 3 (@3) ) - это 2
+    for i in range(1, limit+1):
+      idcg += 1/np.log2(i+1)
+    return idcg
+    raise NotImplementedError()
+    return ans
+
+# NormalizedDiscountedCumulativeGain@3
+def ndcg_at_3() -> float:
+    idcg = idcg_at_3_for_user_2()
+    dcg = dcg_at_3_for_user_2()
+    return dcg/idcg if idcg>0 else 0.0
+    raise NotImplementedError()
+    return ans
